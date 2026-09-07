4683d61885af5c925c9d412dc225c207e8d4defe
kontroldev <raulgallego79@icloud.com>
2026-09-07 21:28:17 +0200

    Expand Ignite footer credit

--- Sources/RaulGallegoPortfolio/Generator.swift
+++ Sources/RaulGallegoPortfolio/Generator.swift
@@ -4,7 +4,7 @@ import Ignite
 struct SiteLayout: Layout {
     var body: some Document {
         Head {
-            MetaLink(href: "/css/main.css?v=20260907-7", rel: .stylesheet)
+            MetaLink(href: "/css/main.css?v=20260907-8", rel: .stylesheet)
             MetaLink(href: "/favicon.svg", rel: .icon)
             MetaTag(name: "theme-color", content: "#f5f5f7")
             MetaTag(name: "color-scheme", content: "light dark")
